4d59fd09aabdba19f55391a3e08e2d6ceb322a2b
Arturo <arturvargas9226@gmail.com>
2019-12-09 14:03:53 -0600

    [add] fetchEvents

---
+++ src/store/api/actions.js
@@ -0,0 +1,26 @@
+import { axiosInstance } from '../../boot/axios';
+
+export function fetchSpeakers({ commit }) {
+  return new Promise((resolve, reject) => {
+    axiosInstance.get('speaker/')
+      .then((result) => {
+        commit('setSpeakers', result.data);
+        resolve(result.data);
+      }).catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export function fetchEvents({ commit }) {
+  return new Promise((resolve, reject) => {
+    axiosInstance.get('conference/')
+      .then((result) => {
+        commit('setEvents', result.data);
+        resolve(result.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
