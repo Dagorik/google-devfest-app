05bed1d0d0f9ca527eeafc76f488a5939584fd72
Arturo <arturvargas9226@gmail.com>
2019-12-09 13:59:31 -0600

    [add] method

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -2,14 +2,14 @@
   <div class="q-pa-lg">
     <div class="row q-gutter-md q-mb-sm">
       <q-timeline :layout="layout" :side="side" color="secondary">
-        <q-timeline-entry heading>Agenda</q-timeline-entry>
-        <q-timeline-entry title="Evento" :subtitle="item.hour" side="left"
-          v-for="(item, index) of events" :key="index">
-            <div class="item-content q-py-sm" v-for="event of item.list" :key="event.id">
+        <q-timeline-entry heading class="font">Agenda</q-timeline-entry>
+        <q-timeline-entry title="Evento" :subtitle="event.start_at" side="left"
+          v-for="(event, index) of events" :key="index">
+            <div class="item-content q-py-sm">
               <div>
                 <q-chip color="product" size="md">{{ event.track }}</q-chip>
-                <span>{{ event.title }}</span>
-                <p> {{ event.speaker }}</p>
+                <span>{{ event.name }}</span>
+                <p> {{ event.speaker[0] }}</p>
               </div>
               <div>
                 <span>Sala: {{ event.place }}</span> - {{ event.seats_avaliables }} seats left
@@ -28,47 +28,17 @@ export default {
     return {
       layout: 'comfortable',
       side: 'right',
-      events: [
-        {
-          hour: '9:00',
-          list: [
-            {
-              id: 0,
-              track: 'All',
-              speaker: '',
-              title: 'Welcome Keynote',
-              place: 'Auditorio de Postgrado',
-            },
-          ],
-        },
-        {
-          hour: '10:00',
-          list: [
-            {
-              id: 1,
-              track: 'Mobile',
-              speaker: 'Raul Dagorik',
-              title: 'Vuejs & Quasar for pwas',
-              place: 'Auditorio de Posgrado',
-              seats_avaliables: 130,
-            },
-            {
-              id: 2,
-              track: 'Web',
-              speaker: 'Edwino',
-              title: 'React Js for pwas',
-              place: 'Salon 9006',
-              seats_avaliables: 30,
-            },
-          ],
-        },
-      ],
+      events: [],
     };
   },
+  methods: {
+    async loadEvents() {
+      const events = this.$store.getters['api/getEvents'];
+      this.events = events.length !== 0 ? events : await this.$store.dispatch('api/fetchEvents');
+    },
+  },
   mounted() {
-    this.$axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(res => console.log(res));
+    this.loadEvents();
   },
 };
 </script>
