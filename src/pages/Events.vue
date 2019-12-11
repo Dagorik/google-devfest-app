52f1ea7da1ce856d22fd89d51e76ac0686b130b2
Arturo <arturvargas9226@gmail.com>
2019-12-11 15:30:44 -0600

    [add] sorting events

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -3,18 +3,24 @@
     <div class="row q-gutter-md q-mb-sm">
       <q-timeline :layout="layout" :side="side" color="secondary">
         <q-timeline-entry heading class="font">Agenda</q-timeline-entry>
-        <q-timeline-entry title="Evento" :subtitle="event.start_at" side="left"
-          v-for="(event, index) of events" :key="index">
-            <div class="item-content q-py-sm">
-              <div>
-                <q-chip color="product" size="md">{{ event.track }}</q-chip>
-                <span>{{ event.name }}</span>
-                <p> {{ event.speaker[0] }}</p>
-              </div>
-              <div>
-                <span>Sala: {{ event.hall }}</span> - {{ event.seats_avaliables }} seats left
-              </div>
+        <q-timeline-entry
+          title="Evento"
+          :subtitle="event.start_at"
+          side="left"
+          v-for="(event, index) of events"
+          :key="index"
+        >
+          <div class="item-content q-py-sm">
+            <div>
+              <q-chip color="product" size="md">{{ event.track }}</q-chip>
+              <span>{{ event.name }}</span>
+              <p>{{ event.speaker[0].first }} {{ event.speaker[0].last }}</p>
             </div>
+            <div>
+              <span>Sala: {{ event.hall }}</span>
+              - {{ event.seats_avaliables }} seats left
+            </div>
+          </div>
         </q-timeline-entry>
       </q-timeline>
     </div>
@@ -45,16 +51,21 @@ export default {
       this.events.forEach((event, index) => {
         event.speaker.forEach((speakerID) => {
           const spk = this.speakers.find(_speaker => _speaker.id === speakerID);
-          const spkName = spk.first_name + spk.last_name;
-          this.events[index].speaker[0] = spkName.replace(/''/g, ' ');
+          const spkName = { first: spk.first_name, last: spk.last_name };
+          this.events[index].speaker[0] = spkName;
         });
       });
-      this.sortedEvents();
+      this.addHourNum();
     },
     sortedEvents() {
-      const c = this.events.sort((a, b) => a.start_at > b.start_at);
+      this.events.sort((a, b) => a.startAtNum - b.startAtNum);
       console.log(this.events);
-      console.log('Sorted ', c);
+    },
+    addHourNum() {
+      this.events.forEach((event, index) => {
+        this.events[index].startAtNum = parseFloat(event.start_at.replace(':', '.').replace(/[^\d.-]/g, ''));
+      });
+      this.sortedEvents();
     },
   },
   mounted() {
