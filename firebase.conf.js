8713e48b16042ed4c797edd18f476cf787ce74f9
Arturo <arturvargas9226@gmail.com>
2019-12-13 18:09:56 -0600

    [upload] firebaseconf

---
+++ firebase.conf.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  apiKey: 'AIzaSyBRPNotOHiVYIW1XKQSw7YKSm79rpw1Q28',
+  authDomain: 'devfest-a8928.firebaseapp.com',
+  databaseURL: 'https://devfest-a8928.firebaseio.com',
+  projectId: 'devfest-a8928',
+  storageBucket: 'devfest-a8928.appspot.com',
+  messagingSenderId: '631808106825',
+  appId: '1:631808106825:web:7dd76ac4ef2b08616cfa98',
+};
