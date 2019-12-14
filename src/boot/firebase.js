e3c152905a4f137ba9d26826873e071b30ead2f7
Arturo <arturvargas9226@gmail.com>
2019-12-13 18:37:44 -0600

    [add] credentials

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,10 +1,19 @@
-import Firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import fireConfig from '../../firebase.conf.js';
 
 
 // Initialize Firebase
-const firebaseApp = Firebase.initializeApp(fireConfig);
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRPNotOHiVYIW1XKQSw7YKSm79rpw1Q28',
+  authDomain: 'devfest-a8928.firebaseapp.com',
+  databaseURL: 'https://devfest-a8928.firebaseio.com',
+  projectId: 'devfest-a8928',
+  storageBucket: 'devfest-a8928.appspot.com',
+  messagingSenderId: '631808106825',
+  appId: '1:631808106825:web:7dd76ac4ef2b08616cfa98',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAuth = firebaseApp.auth();
 
 export { firebaseAuth };
